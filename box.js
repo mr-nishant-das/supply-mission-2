731f3eb1c0898363ccb6450d67f99dd3c8dcef68
mr-nishant-das <70682938+mr-nishant-das@users.noreply.github.com>
2020-09-22 16:15:31 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,10 @@
+class box{
+    constructor(x, y, width, height){
+        var options = {
+            'restitution': 1.0,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body
+    }
+}
